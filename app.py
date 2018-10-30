d06c05ae8cdd39c7ee741cf33971915761ceb020
Shelton Gayles <sheltong@jakks.net>
2018-10-30 15:30:44 -0700

    user login working

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin
+
 
 app = Flask(__name__)
 
@@ -11,21 +15,26 @@ app.config['SECRET_KEY'] = 'e94305dcd6e2a80c903dd5d81bd69849'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
-comments = db.Table('comments',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('comment_id', db.Integer, db.ForeignKey('comment.id'))
-)
+
+# comments = db.Table('comments',
+#     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#     db.Column('comment_id', db.Integer, db.ForeignKey('comment.id'))
+# )
 
 
-notes = db.Table('notes',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('note_id', db.Integer, db.ForeignKey('note.id'))
-)
+# notes = db.Table('notes',
+#     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+#     db.Column('note_id', db.Integer, db.ForeignKey('note.id'))
+# )
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(64), nullable=False)
     last_name = db.Column(db.String(64), nullable=False)
@@ -61,8 +70,10 @@ class Idea(db.Model):
     secondary_document = db.Column(db.String(20))
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comments = db.relationship('Comment', secondary='comments', backref=db.backref('idea_comments', lazy='dynamic'))
-    notes = db.relationship('Note', secondary='notes', backref=db.backref('idea_notes', lazy='dynamic'))
+    # comments = db.relationship('Comment', secondary=comments, 
+    #     backref=db.backref('idea_comments', lazy='dynamic'))
+    # notes = db.relationship('Note', secondary=notes, 
+    #     backref=db.backref('idea_notes', lazy='dynamic'))
 
     def __repr__(self):
         return f"Idea('{self.title}', '{self.id}')"
@@ -118,6 +129,10 @@ ideas = [
 
 ]
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/')
 def index():
@@ -142,7 +157,18 @@ def terms():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(first_name=form.first_name.data, last_name=form.last_name.data, 
+                email=form.email.data, password=hashed_password, 
+                address1=form.address1.data, address2=form.address2.data, 
+                city=form.city.data, state=form.state.data, 
+                postal_code=form.postal_code.data, country=form.country.data,
+                phone=form.phone.data, nda=0,
+                role=0)
+        db.session.add(user)
+        db.session.commit()        
         flash(f'Account created for {form.first_name.data} {form.last_name.data}', 'success')
         return redirect(url_for('home'))
 
@@ -153,12 +179,14 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash(f'Welcome back {user.first_name}', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login unsuccessful. Please check your email and password', 'danger')
-
+            flash('Login Unsuccessful. Please check your email and pasword', 'danger')
     return render_template('login.html', form=form, title='Login')
 
 
